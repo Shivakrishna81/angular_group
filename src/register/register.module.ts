c184f772f2ae93dc7191b89f1b36b20e02b14190
prasadyalla2002 <149290187+prasadyalla2002@users.noreply.github.com>
2024-10-09 16:39:37 +0530

    register page finished with validation

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -10,7 +10,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     RegisterPageComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports:[
     RegisterPageComponent
